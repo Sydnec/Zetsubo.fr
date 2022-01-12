992be50bcd31998608d257aa25753fa545afaef3
Sydnec <97565235+Sydnec@users.noreply.github.com>
2022-01-13 00:26:11 +0100

    Ajout lecteur de musique

--- script/script.js
+++ script/script.js
@@ -1,1 +1,11 @@
-function PlayMusic() {}
+function PlayMusic() {
+    document.getElementById("music-bar").play();
+    document.getElementById("play-button").style.display = "none";
+    document.getElementById("pause-button").style.display = "inline-block";
+}
+
+function PauseMusic() {
+    document.getElementById("music-bar").pause();
+    document.getElementById("pause-button").style.display = "none";
+    document.getElementById("play-button").style.display = "inline-block";
+}
