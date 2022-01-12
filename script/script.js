4f576855b38d0b9d68c32100f9aa38eaab084649
Sydnec <97565235+Sydnec@users.noreply.github.com>
2022-01-13 00:56:39 +0100

    add 2ns music

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
+function shuffleArray() {
+    self.sort(() => Math.random() - 0.5);
+}
+
 function PlayMusic() {
     document.getElementById("music-bar").play();
     document.getElementById("play-button").style.display = "none";
@@ -8,4 +12,8 @@ function PauseMusic() {
     document.getElementById("music-bar").pause();
     document.getElementById("pause-button").style.display = "none";
     document.getElementById("play-button").style.display = "inline-block";
+}
+function NextMusic() {
+    let player = console.log(document.getElementById("music-bar"));
+    let actual = player.getAttribute("src");
 }
